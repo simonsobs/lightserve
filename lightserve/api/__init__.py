909d0f163f2023310cbde2e34f751e4b86c675b9
Josh Borrow <josh@joshborrow.com>
2025-02-28 15:17:51 -0500

    Add renderer for cutouts

--- lightserve/api/__init__.py
+++ lightserve/api/__init__.py
@@ -4,6 +4,7 @@ Main API App for lightserve.
 
 from fastapi import FastAPI
 
+from .cutouts import cutouts_router
 from .lightcurves import lightcurves_router
 from .sources import sources_router
 
@@ -11,3 +12,4 @@ app = FastAPI(root_path="/api/v1")
 
 app.include_router(lightcurves_router)
 app.include_router(sources_router)
+app.include_router(cutouts_router)
--- lightserve/api/cutouts.py
+++ lightserve/api/cutouts.py
@@ -1,3 +1,133 @@
 """
 Endpoints to get cutouts corresponding to specific observations.
 """
+
+import io
+from pathlib import Path
+from typing import Any, BinaryIO, Optional, Union
+
+import matplotlib.pyplot as plt
+import numpy as np
+from fastapi import APIRouter, Depends, HTTPException, Response, status
+from lightcurvedb.client.cutouts import CutoutNotFound, cutout_read_from_flux_id
+from matplotlib.colors import LogNorm
+from pydantic import BaseModel, Field
+
+from lightserve.database import AsyncSessionDependency
+
+cutouts_router = APIRouter(prefix="/cutouts")
+
+
+class RenderOptions(BaseModel):
+    cmap: str = Field(default="viridis")
+    "Color map to use for rendering, defaults to 'viridis', and may not be used if RGBA buffers are provided."
+    vmin: float = Field(default=0.0)
+    "Color map range minimum, defaults to -100"
+    vmax: float = Field(default=0.1)
+    "Color map range maximum, defaults to 100"
+    log_norm: bool = Field(default=False)
+    "Whether to use a log normalization, defaults to False."
+    clip: bool = Field(default=True)
+    "Whether to clip values outside of the range, defaults to True."
+
+    @property
+    def norm(self) -> plt.Normalize:
+        if self.log_norm:
+            return LogNorm(vmin=self.vmin, vmax=self.vmax, clip=self.clip)
+        else:
+            return plt.Normalize(vmin=self.vmin, vmax=self.vmax, clip=self.clip)
+
+
+class Renderer:
+    format: Optional[str]
+    "Format to render images to, defaults to 'webp'."
+    pil_kwargs: Optional[dict[str, Any]]
+    "Keyword arguments to pass to PIL for rendering, defaults to None."
+
+    def __init__(
+        self,
+        format: Optional[str] = "webp",
+        pil_kwargs: Optional[dict[str, Any]] = None,
+    ):
+        self.format = format
+        self.pil_kwargs = pil_kwargs
+
+        return
+
+    def render(
+        self,
+        fname: Union[str, Path, BinaryIO],
+        buffer: np.ndarray,
+        render_options: RenderOptions,
+    ):
+        """
+        Renders the buffer to the given file.
+
+        Parameters
+        ----------
+        fname : Union[str, Path, BinaryIO]
+            Output for the rendering.
+        buffer : np.ndarray
+            Buffer to render to disk or IO.
+        render_options : RenderOptions
+            Options for rendering.
+
+        Notes
+        -----
+
+        Buffer is transposed in x, y to render correctly within this function.
+        """
+
+        if buffer.ndim == 2:
+            # Render with colour mapping, this is 'raw data'.
+            cmap = plt.get_cmap(render_options.cmap)
+            cmap.set_bad("#dddddd", 0.0)
+            plt.imsave(
+                fname,
+                render_options.norm(buffer),
+                cmap=cmap,
+                pil_kwargs=self.pil_kwargs,
+                format=self.format,
+                vmin=0.0,
+                vmax=1.0,
+                origin="lower",
+            )
+        else:
+            # Direct rendering
+            plt.imsave(
+                fname,
+                np.ascontiguousarray(buffer.swapaxes(0, 1)),
+                pil_kwargs=self.pil_kwargs,
+                format=self.format,
+            )
+
+        return
+
+
+render_options = RenderOptions()
+renderer = Renderer(format="png")
+
+
+@cutouts_router.get("/flux/{id}")
+async def cutouts_get_from_flux_id(
+    id: int,
+    conn: AsyncSessionDependency,
+    render_options: RenderOptions = Depends(RenderOptions),
+) -> Response:
+    """
+    Return the cutout assocaited with a flux measurement's ID.
+    """
+
+    try:
+        cutout = await cutout_read_from_flux_id(flux_measurement_id=id, conn=conn)
+    except CutoutNotFound:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Cutout not found for flux measurement {id}",
+        )
+
+    numpy_buf = np.array(cutout.data)
+
+    with io.BytesIO() as output:
+        renderer.render(output, numpy_buf, render_options=render_options)
+        return Response(content=output.getvalue(), media_type="image/png")
