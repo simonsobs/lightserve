9c07960267336a4b89ae3691975bebe1e0482ba8
Josh Borrow <josh@joshborrow.com>
2025-04-30 15:55:49 -0400

    Minor fixes

--- launch.sh
+++ launch.sh
@@ -2,7 +2,7 @@
 
 lightcurvedb-setup
 
-uvicorn lightserve.api:app --port 8000 &
-uvicorn lightgest.api:app --port 8001 &
+uvicorn lightserve.api:app --port 8000 --host 0.0.0.0 &
+uvicorn lightgest.api:app --port 8001 --host 0.0.0.0 &
 
 wait
