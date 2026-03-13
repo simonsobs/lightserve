eb4f3207a6c4279e93dba2d3140b913edd63aaac
Josh Borrow <josh@joshborrow.com>
2026-03-13 14:18:18 -0400

    Added ability to run lightgest from lightserve-ephemeral

--- lightserve/scripts/ephemeral.py
+++ lightserve/scripts/ephemeral.py
@@ -2,16 +2,38 @@
 Run an ephemeral server, alongside an ephemeral lightcurve server.
 """
 
+import multiprocessing as mp
+
 import uvicorn
 from lightcurvedb.cli.ephemeral import core as db
 
 
-def core(number: int = 16, backend: str = "postgres"):
+def run_server(app: str, port: int):
+    uvicorn.run(app, port=port, log_level="info", reload=True)
+
+
+def make_process(app: str, port: int):
+    p = mp.Process(target=run_server, args=(app, port))
+    p.start()
+
+    return p
+
+
+def setup_servers(run_ingest: bool = False):
+    processes = [make_process("lightserve.api:app", 8000)]
+
+    if run_ingest:
+        processes.append(make_process("lightgest.api:app", 8001))
+
+    for p in processes:
+        p.join()
+
+
+def core(number: int = 16, backend: str = "postgres", run_ingest: bool = False):
     # Setup that DB
     with db(number=number, probability_of_flare=0.9, backend_type=backend):
         print("Starting webapp")
-
-        uvicorn.run("lightserve.api:app", reload=True)
+        setup_servers(run_ingest)
 
 
 def main():
@@ -31,9 +53,14 @@ def main():
         choices=["postgres", "timescale", "parquet"],
         default="postgres",
     )
+    parser.add_argument(
+        "--run-ingest",
+        action="store_true",
+        help="If this parameter is provided, we also run the lightgest server.",
+    )
 
     args = parser.parse_args()
-    core(args.number, args.backend)
+    core(args.number, args.backend, args.run_ingest)
 
 
 if __name__ == "__main__":
