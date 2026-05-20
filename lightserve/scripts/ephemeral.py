c496b036a4cf9f339ddad8ff71c2a0ca704a435a
Josh Borrow <josh@joshborrow.com>
2026-05-20 11:13:10 -0400

    Allow multiple threads in ephemeral for testing

--- lightserve/scripts/ephemeral.py
+++ lightserve/scripts/ephemeral.py
@@ -10,28 +10,28 @@ from lightcurvedb.cli.ephemeral import core as db
 from lightserve.telemetry import start_jaeger
 
 
-def run_server(app: str, port: int):
-    uvicorn.run(app, port=port, log_level="info", reload=True)
+def run_server(app: str, port: int, workers: int):
+    uvicorn.run(app, port=port, log_level="info", reload=workers == 1, workers=workers)
 
 
-def make_process(app: str, port: int):
-    p = mp.Process(target=run_server, args=(app, port))
+def make_process(app: str, port: int, workers: int):
+    p = mp.Process(target=run_server, args=(app, port, workers))
     p.start()
 
     return p
 
 
-def setup_servers(run_ingest: bool = False):
-    processes = [make_process("lightserve.api:app", 8000)]
+def setup_servers(run_ingest: bool = False, workers: int = 1):
+    processes = [make_process("lightserve.api:app", 8000, workers)]
 
     if run_ingest:
-        processes.append(make_process("lightgest.api:app", 8001))
+        processes.append(make_process("lightgest.api:app", 8001, workers))
 
     for p in processes:
         p.join()
 
 
-def core(number: int = 16, backend: str = "postgres", run_ingest: bool = False):
+def core(number: int = 16, backend: str = "postgres", run_ingest: bool = False, workers: int = 1):
     jaeger, ui_url, otlp_endpoint = start_jaeger()
     try:
         # Setup that DB
@@ -41,7 +41,7 @@ def core(number: int = 16, backend: str = "postgres", run_ingest: bool = False):
             backend_type=backend,
         ):
             print("Starting webapp")
-            setup_servers(run_ingest)
+            setup_servers(run_ingest, workers)
     finally:
         jaeger.stop()
 
@@ -68,9 +68,20 @@ def main():
         action="store_true",
         help="If this parameter is provided, we also run the lightgest server.",
     )
+    parser.add_argument(
+        "-w",
+        "--workers",
+        type=int,
+        default=1,
+        help=(
+            "Number of workers to use for the FastAPI servers. Note that using more than 1 worker "
+            "will cause issues with the in-memory backend, and may cause issues with the parquet "
+            "backend as well. Use with caution. Reloading is disabled when workers > 1."
+        ),
+    )
 
     args = parser.parse_args()
-    core(args.number, args.backend, args.run_ingest)
+    core(args.number, args.backend, args.run_ingest, args.workers)
 
 
 if __name__ == "__main__":
